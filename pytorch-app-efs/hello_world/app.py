c9a1ce9a2623e41c2fee3830e8e4075cc3665849
EC2 Default User <ec2-user@ip-172-31-93-13.ec2.internal>
2021-06-25 03:56:30 +0000

    first commit

---
+++ pytorch-app-efs/hello_world/app.py
@@ -0,0 +1,44 @@
+import json
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+import torch
+import os 
+
+FOLDER = "/mnt/model/"
+
+print(os.listdir(FOLDER))
+
+def lambda_handler(event, context):
+
+    tokenizer = AutoTokenizer.from_pretrained(FOLDER)
+    model = AutoModelForQuestionAnswering.from_pretrained(FOLDER)
+    
+    body = json.loads(event['body'])
+
+    question = body['question']
+    context = body['context']
+
+    inputs = tokenizer.encode_plus(question, context,add_special_tokens=True, return_tensors="pt")
+    input_ids = inputs["input_ids"].tolist()[0]
+
+    output = model(**inputs)
+    answer_start_scores = output.start_logits
+    answer_end_scores = output.end_logits
+
+    answer_start = torch.argmax(answer_start_scores)
+    answer_end = torch.argmax(answer_end_scores) + 1
+
+    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+
+    print('Question: {0}, Answer: {1}'.format(question, answer))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'Question': question,
+            'Answer': answer
+        })
+    }
+    
+    
+    
+
